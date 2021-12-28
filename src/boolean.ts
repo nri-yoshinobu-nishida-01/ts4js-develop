5beef1efd38b54b2a2111e809e6c4471bd723360
Yoshinobu Nishida <y-nishida@nri.co.jp>
2021-12-28 22:05:12 +0900

    added number-string.ts

---
+++ src/boolean.ts
@@ -0,0 +1,12 @@
+export {};
+
+// how to run
+// ts-node-dev --respawn "src/boolean.ts"
+
+let name = 'TypeScript';
+
+let isFinished: boolean = true;
+isFinished = false;
+//isFinished = 1;
+console.log({ isFinished });
+
